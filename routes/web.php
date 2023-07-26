77b5e54a13d330563aac7dac814130d6b60f945a
Hanna Khan <47082795+Hana-Khan@users.noreply.github.com>
2023-07-26 22:45:28 +0500

    routes added

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+//auth()->loginUsingId(1);
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware('auth')->group(function(){
+    Route::get('/tweets', 'TweetsController@index')->name('home');
+    Route::post('/tweets', 'TweetsController@store');
+    Route::post('/profiles/{user}/follow', 'FollowsController@store');
+    Route::get('/profiles/{user}/edit', 'profilesController@edit')->middleware('can:edit,user');
+
+});
+
+Route::get('/profiles/{user}', 'profilesController@show')->name('profile');
+
+// Route::get('/profiles/{user:name}', 'ProfilesController@show')->name('profile');
+
+Auth::routes();
+
